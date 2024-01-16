adc9e8b4249b548ad8c052fca567859e29c7e820
Lequocluan <quocluan15102003@gmail.com>
2024-01-16 11:05:39 +0700

    Cập nhật
    Sửa và thêm mới

--- HelloWorld/src/XinChao.java
+++ HelloWorld/src/XinChao.java
@@ -12,7 +12,9 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tôi là Lê Quốc Luân");
+		System.out.println("Tôi là Lê Quốc Luân, lớp 63.CNTT");
+		//them dong
+		System.out.println("Thêm dòng này và sửa dòng trên");
 	}
 
 }
