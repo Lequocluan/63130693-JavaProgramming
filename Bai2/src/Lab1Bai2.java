cabe1af82ccd1bfc333cc73c539b7b1ad2fed698
Lequocluan <quocluan15102003@gmail.com>
2024-01-20 10:17:55 +0700

    bài 2
    tính chu vi

---
+++ Bai2/src/Lab1Bai2.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+
+import java.util.Scanner;
+/**
+ * 
+ * 
+ */
+public class Lab1Bai2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("nhap chieu dai: ");
+		int chieuDai=scanner.nextInt();
+		System.out.print("nhap chieu rong: ");
+		int chieuRong=scanner.nextInt();
+		System.out.printf("chu vi hinh chu nhat la %d",(chieuDai+chieuRong)*2);
+	}
+
+}
