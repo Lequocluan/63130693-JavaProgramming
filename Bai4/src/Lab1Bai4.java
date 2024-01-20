5debcb731cf66b92ba552d2eb0207251274ff7e1
Lequocluan <quocluan15102003@gmail.com>
2024-01-20 11:20:25 +0700

    bài 4
    lần 2
    xử lý khi delta < 0

--- Bai4/src/Lab1Bai4.java
+++ Bai4/src/Lab1Bai4.java
@@ -14,6 +14,7 @@ public class Lab1Bai4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("phương trình bậc 2");
 		System.out.print("nhập hệ số a: ");
 		int a =  scanner.nextInt();
 		System.out.print("nhập hệ số b: ");
@@ -23,6 +24,11 @@ public class Lab1Bai4 {
 		
 		//khai báo delta
 		double delta = Math.pow(b, 2)-4*a*c;
+		if(delta<0) {
+			System.out.println("delta < 0( ptvn)");
+		}else {
+			System.out.println("căn bậc 2 delta bằng " + Math.sqrt(delta));
+		}
 	}
 
 }
