490522bc7e8d421493eb3debca45b4df5085aad2
Lequocluan <quocluan15102003@gmail.com>
2024-01-20 10:57:13 +0700

    bài 3
    tính diện tích khối lập phương

---
+++ Bai3/src/Lab1Bai3.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Lab1Bai3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("nhập cạnh của khối lập phương: ");
+		int canh = scanner.nextInt();
+		System.out.println("diện tích của khối lập phương là: " + canh*canh*canh);
+	}
+
+}
