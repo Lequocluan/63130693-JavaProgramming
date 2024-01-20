b06e7e615d6fff5cf4a3a2722789697152b550c8
Lequocluan <quocluan15102003@gmail.com>
2024-01-20 10:59:13 +0700

    bài 3
    dùng hàm để tính thể tích khối lập phương

--- Bai3/src/Lab1Bai3.java
+++ Bai3/src/Lab1Bai3.java
@@ -18,6 +18,8 @@ public class Lab1Bai3 {
 		System.out.print("nhập cạnh của khối lập phương: ");
 		int canh = scanner.nextInt();
 		System.out.println("diện tích của khối lập phương là: " + canh*canh*canh);
+		int dienTich= (int)Math.pow(canh, 3);
+		System.out.println("diện tích của khối lập phương( dùng hàm) là: " + dienTich);
 	}
 
 }
