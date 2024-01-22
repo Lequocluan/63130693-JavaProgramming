45f2307b0d3dc3d4631aeff26322668e71bf4392
Lequocluan <quocluan15102003@gmail.com>
2024-01-22 08:46:49 +0700

    Update Lab2Bai3.java

--- Lab2/src/Lab2Bai3.java
+++ Lab2/src/Lab2Bai3.java
@@ -2,6 +2,7 @@
  * 
  */
 
+import java.util.Scanner;
 /**
  * 
  */
@@ -12,7 +13,17 @@ public class Lab2Bai3 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int soDien;
+		do {
+			System.out.print("nhap so dien su dung > 0: ");
+			 soDien = sc.nextInt();
+		}while(soDien<0);
+		if(soDien<50) {
+			System.out.println("so tien la: "+soDien*1000);
+		}else {
+			System.out.println("so tien la: "+(50*1000 + (soDien-50)*1200));
+		}
 	}
 
 }
